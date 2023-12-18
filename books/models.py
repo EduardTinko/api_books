4800a6d6b0364931e5c4924cce4ba1bae92a1dba
TinkoEd <228rubiks228@gmail.com>
2023-12-19 00:59:58 +0200

    update tests and validators

--- books/models.py
+++ books/models.py
@@ -1,14 +1,20 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 # Create your models here.
 
 
+def validate_name(value):
+    if len(value) <= 1:
+        raise ValidationError(f"Занадто коротке значення")
+
+
 class Author(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, validators=[validate_name])
 
 
 class Book(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, validators=[validate_name])
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="book")
-    genre = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50, validators=[validate_name])
     publication_date = models.DateField()
